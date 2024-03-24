244d3ef5fd35f249b98aacee65124dd5e77faead
MNatiki <natiart07@gmail.com>
2024-03-24 08:32:50 +0300

    auth

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'feriufkgjbbvidojfkvrfuiljkbmfirkfmhk';
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://alx-blog:pXrGAJdZD2uPllMM@cluster0.7xciobw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -25,5 +27,35 @@ app.post('/register', async (req,res) => {
       res.status(400).json(e);
     }
   });
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('Wrong Information');
+    }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
 
 app.listen(4000);
