16a13c6efefdcf813bc476ca2933165df178a948
MNatiki <natiart07@gmail.com>
2024-03-27 15:36:44 +0300

    update web post editor file

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -1,6 +1,13 @@
-import ReactQuill from "react-quill";
+import ReactQuill from "react-quill"; // Import ReactQuill component for rich text editing
 
-export default function Editor({value,onChange}) {
+/**
+ * Editor component for rich text editing using ReactQuill.
+ * @param {string} value - The current value of the editor.
+ * @param {function} onChange - Function to handle changes in the editor content.
+ * @returns {JSX.Element} - Returns the Editor component JSX.
+ */
+export default function Editor({ value, onChange }) {
+  // Define modules for toolbar customization
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
@@ -15,13 +22,17 @@ export default function Editor({value,onChange}) {
       ['clean'],
     ],
   };
+
+  // Render the Editor component 
   return (
     <div className="content">
-    <ReactQuill
-      value={value}
-      theme={'snow'}
-      onChange={onChange}
-      modules={modules} />
+      {/* ReactQuill component for rich text editing */}
+      <ReactQuill
+        value={value} // Current value of the editor
+        theme={'snow'} // Theme for the editor
+        onChange={onChange} // Function to handle changes in the editor content
+        modules={modules} // Modules for toolbar customization
+      />
     </div>
   );
 }
