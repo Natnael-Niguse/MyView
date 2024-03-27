3c6d0e9fd42f0dfebfd1f1eb1b09a8a7300d0342
MNatiki <natiart07@gmail.com>
2024-03-27 15:38:50 +0300

    update web post usercontext file

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -1,11 +1,20 @@
-import {createContext, useState} from "react";
+import { createContext, useState } from "react";
 
+// Create a context for user information
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}) {
-  const [userInfo,setUserInfo] = useState({});
+/**
+ * UserContextProvider component to provide user information context to its children.
+ * @param {object} children - The children components to be wrapped by the context provider.
+ * @returns {JSX.Element} - Returns the UserContextProvider component JSX. 
+ */
+export function UserContextProvider({ children }) {
+  // State for storing user information
+  const [userInfo, setUserInfo] = useState({});
+
   return (
-    <UserContext.Provider value={{userInfo,setUserInfo}}>
+    // Provide user information context to children components
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
