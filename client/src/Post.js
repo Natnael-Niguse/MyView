336244be593b3ed5467a08507070b174fe8d03f2
MNatiki <natiart07@gmail.com>
2024-03-27 07:42:34 +0300

    myview blog page

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-export default function Post() {
-    return (
-        <div className="post">
-        <div className="image">
-          <img src="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"></img>
-        </div>
-        <div className="text">
-        <h2>The dog is a domesticated descendant of the wolf</h2>
-        <p className="info">
-          <a className="author">Natnael Niguse</a>
-          <time>2024-03-22</time>
-        </p>
-        <p className="summary">The dog is a pet animal. A dog has sharp teeth so that it can eat flesh very easily, it has four legs, two ears, two eyes, a tail, a mouth, and a nose. It is a very clever animal and is very useful in catching thieves. It runs very fast, barks loudly and attacks the strangers. </p>
-        </div>
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
       </div>
-    );
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
