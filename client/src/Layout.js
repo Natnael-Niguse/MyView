234d1ac6a781f076cef995bdb864ba434f249949
MNatiki <natiart07@gmail.com>
2024-03-27 15:37:42 +0300

    update web post layout file

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,10 +1,16 @@
-import Header from './Header'
-import { Outlet } from "react-router-dom";
+import Header from './Header'; // Import Header component
+import { Outlet } from "react-router-dom"; // Import Outlet component from react-router-dom
 
+/**
+ * Layout component for rendering header and nested routes.
+ * @returns {JSX.Element} - Returns the Layout component JSX. 
+ */
 export default function Layout() {
     return (
         <main>
+            {/* Render the Header component */}
             <Header />
+            {/* Render the nested routes */}
             <Outlet />
         </main>
     );
